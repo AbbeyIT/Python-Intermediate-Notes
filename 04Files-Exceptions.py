c7c80c86bbf001f4b5d2216b862119ffe643b1ca
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-05-31 00:00:59 +0800

    Modified files exceptions, added more notes

--- 04Files-Exceptions.py
+++ 04Files-Exceptions.py
@@ -1,4 +1,4 @@
-#Handling Creating Writing to a File
+# === Handling Creating Writing to a File === #
 
 #Files
 """
@@ -38,7 +38,8 @@ f.close()
 
 """
 
-#Reading from a file
+
+# === Reading from a file === #
 """
 r(read) - default value for python 
 FileNotFoundError - if the file doesn't exist
@@ -82,4 +83,72 @@ else:
 >if sure the file exists
 import os
 os.remove("newfile.txt")
+"""
+
+
+# === Exceptions === #
+"""
+Exception - is an object which tell you about the problem you encountered. It also provibes a traceback, which tells
+you specifically where you made a blunder in your code. 
+
+- Built-in excetions -
+1. raise keywoard - lets us throw an excetion. We can use it to define the error we'd like to raise, and what text
+will be printed on the console
+Example: we dont want any negative values
+n = -5
+if n < 0:
+    raise Exception("No negative numbers allowed")
+
+2. syntax error - occurs when the code is not written correctly
+
+3. assertions - boolean expressions that make sure that the conditions are true.
+Example: 
+employee_of_the_year = "Roger"
+
+assert employee_of_the_year == "Roger"
+#can also be used for debugging tool because its stops the program as soon as an error occur
+assert employee_of_the_year == "Shane" #error because we also assign shane as employee of the year
+
+4. ZeroDivisionError - occurs when you divide by zero
+Example: 
+1/0
+
+5. TypeError - occurs when an operation is performed on an incorrect or unsupported object type. 
+Example: 
+50 ** "two"
+
+- How we can handle them? - 
+try except - these blocks are used to spot and handle exceptions
+try - test the code to ensure that there are no errors
+except - if we don't handle an exception with an except block, the program will crash
+finally - finally block allows us to run our code no matter what the result of the try and except blocks is
+
+Example: 
+try: 
+    print(random_var)
+except:
+    print("Exception Alert!")
+
+try:
+    print(x + "macarons")
+except NameError:
+    print("Please define your variable")
+except IndentationError:
+    print("Please be careful with your identing your code.")
+except:
+    print("Something else went wrong. We need to figure it out.")
+
+try:
+    print(24 + 42)
+except:
+    print("This operation cannot be performed")
+else:
+    print("No issues here!")
+
+try: 
+    print(n)
+except:
+    print("There something wrong with our program.")
+finally:
+    print("Let's run our program anyway") #run our code even there's error
 """
